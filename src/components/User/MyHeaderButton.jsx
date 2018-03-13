c3296a65473c9459dcb8fe220d621a476668065b
ashinzekene <ashinzekene@gmail.com>
2018-03-13 21:17:01 +0100

    implemented summy follow

--- src/components/User/MyHeaderButton.jsx
+++ src/components/User/MyHeaderButton.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Button from "semantic-ui-react/dist/commonjs/elements/Button/Button";
 import Link from 'react-router-dom/Link';
 
-const MyHeaderButton =({isCurrentUser, currentUserIsFollowing }) => {
+const MyHeaderButton =({ user, isCurrentUser, currentUserIsFollowing }) => {
+  const followOrUnfollow = () => {
+    if (currentUserIsFollowing) {
+      console.log("I would stop following you")
+    } else {
+      console.log("I would start following you")
+    }
+  }
   let myButton
   if (isCurrentUser) {
     myButton = <Button floated="right" size="large" as={ Link } to="/settings" content="Edit Profile"/> 
   } else {
-    myButton = <Button floated="right" size="large" content={ currentUserIsFollowing ? "Unfollow" : "Follow" }/>
+    myButton = <Button onClick={ followOrUnfollow } floated="right" size="large" content={ currentUserIsFollowing ? "Unfollow" : "Follow" }/>
   }
 
   return (
